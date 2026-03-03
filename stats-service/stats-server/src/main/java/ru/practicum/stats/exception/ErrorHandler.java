563ec5b948a706e07241fcd6870f392295f26968
Jarvis <jarvis424@gmail.com>
2026-03-03 20:35:12 +0300

    ErrorHandler fix

--- stats-service/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.stats.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -32,5 +33,11 @@ public class ErrorHandler {
     public Map<String, String> handleThrowable(final Throwable e) {
         log.error("Internal error: {}", e.getMessage(), e);
         return Map.of("error", "Произошла непредвиденная ошибка");
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMissingParams(MissingServletRequestParameterException e) {
+        return Map.of("error", "Отсутствует обязательный параметр: " + e.getParameterName());
     }
 }
